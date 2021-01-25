2dfca38b0ee1c10c7ce3e4111aa57cc524a819a7
sebasvalencia726 <sebasvalencia726@gmail.com>
2021-01-25 15:24:14 -0500

    correction static folder

--- backend/src/app.py
+++ backend/src/app.py
@@ -11,7 +11,7 @@ from flask_pymongo import PyMongo
 from bson import ObjectId
 
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='pdf')
 app.config['MONGO_URI'] = 'mongodb://localhost/pythonreact'
 mongo = PyMongo(app)
 db = mongo.db.pythonreact
